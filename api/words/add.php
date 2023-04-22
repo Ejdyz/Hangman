4889d4e2a14d319a0b2a2d7ebb4b7952def58153
Th3Vladimir <vladimir.vor@seznam.cz>
2023-04-23 00:29:20 +0200

    feat: word add api /api/words/add

---
+++ api/words/add.php
@@ -0,0 +1,37 @@
+<?php
+
+$conn = mysqli_connect("localhost", "root", "", "wea");
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+session_start();
+// if (!isset($_SESSION['user_id'])) { //todo add login
+//     header("HTTP/1.1 401 Unauthorized");
+//     exit;
+// }
+
+
+if (isset($_POST['word'])) {
+    $word = $_POST['word'];
+} else {
+    header("HTTP/1.1 400 Bad Request");
+    exit;
+}
+
+$stmt = mysqli_prepare($conn, "INSERT INTO words (word) VALUES (?)");
+mysqli_stmt_bind_param($stmt, "s", $word);
+mysqli_stmt_execute($stmt);
+
+if (mysqli_affected_rows($conn) > 0) {
+    echo "The word was added";
+} else {
+    header("HTTP/1.1 500 Internal Server Error");
+    echo "Error";
+}
+
+mysqli_close($conn);
+
+?>
