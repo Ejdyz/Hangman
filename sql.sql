afe673509901a19ad7a4f42947945a5e312338b7
Th3Vladimir <vladimir.vor@seznam.cz>
2023-04-23 15:50:32 +0200

    fix: stats - id

--- sql.sql
+++ sql.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `wea`.`users` (`id` INT(255) NOT NULL AUTO_INCREMENT , `username` VARCHAR(256) NOT NULL , `email` VARCHAR(256) NOT NULL , `password` VARCHAR(256) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
 CREATE TABLE `wea`.`words` (`id` INT(255) NOT NULL AUTO_INCREMENT , `word` VARCHAR(256) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
-CREATE TABLE `wea`.`stats` (`id` INT(255) NOT NULL , `win` BOOLEAN NOT NULL , `user_id` INT(255) NOT NULL , `word_id` INT(255) NOT NULL ) ENGINE = InnoDB;
+CREATE TABLE `wea`.`stats` (`id` INT(255) AUTO_INCREMENT, `win` BOOLEAN NOT NULL , `user_id` INT(255) NOT NULL , `word_id` INT(255) NOT NULL, PRIMARY KEY (`id`) ) ENGINE = InnoDB;
 
 INSERT INTO words (word)
 VALUES ('kabat'),
