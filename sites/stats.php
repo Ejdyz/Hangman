b97d97fc4af16ce6b172d6d3d91f4dbb3b3d9c92
Th3Vladimir <vladimir.vor@seznam.cz>
2023-04-23 15:33:14 +0200

    feat: backend for stats

--- sites/stats.php
+++ sites/stats.php
@@ -1,24 +1,38 @@
 <?php
-  session_start();
+
 $conn = mysqli_connect("localhost", "root", "", "wea");
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql_count = "SELECT COUNT(*) as count FROM words";
-$result_count = mysqli_query($conn, $sql_count);
-$row_count = mysqli_fetch_assoc($result_count);
-$count = $row_count['count'];
+session_start();
 
-$midpoint = ceil($count / 2);
+if (!isset($_SESSION['user_id'])) { //todo add login
+    header("HTTP/1.1 401 Unauthorized");
+    echo "You need to be logged in";
+    exit;
+}
 
-$sql_first_half = "SELECT * FROM words LIMIT $midpoint";
-$result_first_half = mysqli_query($conn, $sql_first_half);
+$user_id = $_SESSION['user_id'];
 
-$sql_second_half = "SELECT * FROM words LIMIT $midpoint, $count";
-$result_second_half = mysqli_query($conn, $sql_second_half);
-?><!DOCTYPE html>
+$query = "SELECT COUNT(*) AS total, SUM(win) AS wins FROM stats WHERE user_id = $user_id"; //todo change
+$result = mysqli_query($conn, $query);
+$row = mysqli_fetch_assoc($result);
+
+$total_games = $row['total'];
+$total_wins = $row['wins'];
+$total_losses = $total_games - $total_wins;
+$win_percent = ($total_games > 0) ? round(($total_wins / $total_games) * 100, 2) : 0;
+
+
+$query = "SELECT w.word, s.win, s.id FROM stats s JOIN words w ON s.word_id = w.id WHERE s.user_id = $user_id ORDER BY s.id DESC"; //todo change
+$result = mysqli_query($conn, $query);
+
+mysqli_close($conn);
+?>
+
+<!DOCTYPE html>
 <html lang="en" data-bs-theme="dark">
 <!-- data-bs-theme="light" for light mode -->
 
@@ -27,8 +41,7 @@ $result_second_half = mysqli_query($conn, $sql_second_half);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 
     <title>Statistics</title>
@@ -42,8 +55,7 @@ $result_second_half = mysqli_query($conn, $sql_second_half);
             <a class="navbar-brand" href="#">
                 Hangman
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
@@ -55,7 +67,7 @@ $result_second_half = mysqli_query($conn, $sql_second_half);
                         <a class="nav-link" href="game.php">Game</a>
                     </li>
 
-                    <?php if (isset($_SESSION["user"])){?>
+                    <?php if (isset($_SESSION["user"])) { ?>
                         <li class="nav-item">
                             <a class="nav-link" href="./words.php">Words</a>
                         </li>
@@ -63,22 +75,22 @@ $result_second_half = mysqli_query($conn, $sql_second_half);
                             <a class="nav-link active" href="./stats.php">Statistics</a>
                         </li>
 
-                <?php } ?>
+                    <?php } ?>
                 </ul>
-                <?php if (isset($_SESSION["user"])){?>
+                <?php if (isset($_SESSION["user"])) { ?>
                     <ul class="navbar-nav ms-auto">
                         <li class="nav-item pull-right">
                             <a class="btn btn-outline-success me-2 disabled" type="button" href="./sites/register.php"><?php echo $_SESSION["user"]  ?></a>
                         </li>
                     </ul>
-                <?php }else{?>
+                <?php } else { ?>
 
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item pull-right">
-                        <a class="btn btn-outline-success me-2" type="button" href="./sites/register.php">Register</a>
-                        <a class="btn btn-sm btn-outline-secondary" type="button" href="./sites/login.php">Login</a>
-                    </li>
-                </ul>
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item pull-right">
+                            <a class="btn btn-outline-success me-2" type="button" href="./sites/register.php">Register</a>
+                            <a class="btn btn-sm btn-outline-secondary" type="button" href="./sites/login.php">Login</a>
+                        </li>
+                    </ul>
                 <?php } ?>
             </div>
         </div>
@@ -86,44 +98,56 @@ $result_second_half = mysqli_query($conn, $sql_second_half);
 
 
     <div class="container" style="margin-top: 30px;">
-		<!-- <h1>Edit Words</h1> -->
-		<div class="row" style="margin-bottom: 30px;">
-			<div class="col-md-12">
-				<form id="add-word-form">
-					<div class="form-group">
-						<h5>Win rate: 50%</h5>
-            <h5>number of games: 80</h5>
-            <h5>number of lost games: 40</h5>
-            <h5>number of win games: 40</h5>
-					</div>
-				</form>
-			</div>
-		</div>
-		<div class="row" style="padding-top: 20px; border-top: 1px solid gray;">
-		<div class="col-md-12">
-			<div class="row">
-				<div class="col-md-12">
-					<table class="table table-bordered text-center">
-						<thead>
-							<tr>
-								<th style="width: 15%;">game id</th>
-                <th style="width: 70%;">Word</th>
-                <th style="width: 15%;">Win/lose</th>
-							</tr>
-						</thead>
-						<tbody>
-              <tr>
-                <td>1</td>
-                <td>Slovicko</td>
-                <td>False</td>
-              </tr>
-						</tbody>
-					</table>
-				</div>
-				
-    </div>
-		</div>
-	</div>
+        <!-- <h1>Edit Words</h1> -->
+        <div class="row" style="margin-bottom: 30px;">
+            <div class="col-md-12">
+                <form id="add-word-form">
+                    <div class="form-group">
+                        <?php
+                        echo "<h5>Win rate: " . $win_percent . "% </h5>";
+                        echo "<h5>number of games: " . $total_games . "</h5>";
+                        echo "<h5>number of lost games: " . $total_losses . "</h5>";
+                        echo "<h5>number of win games: " . $total_wins . "</h5>"
+                        ?>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="row" style="padding-top: 20px; border-top: 1px solid gray;">
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-12">
+                        <table class="table table-bordered text-center">
+                            <thead>
+                                <tr>
+                                    <th style="width: 15%;">game id</th>
+                                    <th style="width: 70%;">Word</th>
+                                    <th style="width: 15%;">Win/Lose</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                if (mysqli_num_rows($result) > 0) {
+                                    while ($row = mysqli_fetch_assoc($result)) {
+                                        $word = $row['word'];
+                                        $win = ($row['win'] == 1) ? 'Win' : 'Lose';
+                                        $id = $row["id"];
+                                        echo "<tr><td>" . $id . "</td><td>" . $word . "</td><td>" . $win . "</td></tr>";
+                                    }
+                                
+                                    echo "</table>";
+                                }
+                                else {
+                                    echo "<tr><td colspan='3'>No games to display</td></tr>";
+                                }
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+
+                </div>
+            </div>
+        </div>
 </body>
 
 </html>
