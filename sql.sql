e753b1f5a0d0951982ea6f0b2e5f4f9b0df83c41
Th3Vladimir <vladimir.vor@seznam.cz>
2023-04-23 14:50:11 +0200

    data: words

--- sql.sql
+++ sql.sql
@@ -1,3 +1,30 @@
 CREATE TABLE `wea`.`users` (`id` INT(255) NOT NULL AUTO_INCREMENT , `username` VARCHAR(256) NOT NULL , `email` VARCHAR(256) NOT NULL , `password` VARCHAR(256) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
 CREATE TABLE `wea`.`words` (`id` INT(255) NOT NULL AUTO_INCREMENT , `word` VARCHAR(256) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
 CREATE TABLE `wea`.`stats` (`id` INT(255) NOT NULL , `win` BOOLEAN NOT NULL , `user_id` INT(255) NOT NULL , `word_id` INT(255) NOT NULL ) ENGINE = InnoDB;
+
+INSERT INTO words (word)
+VALUES ('kabat'),
+       ('prst'),
+       ('praha'),
+       ('auto'),
+       ('kolo'),
+       ('kapka'),
+       ('krab'),
+       ('mrak'),
+       ('stavba'),
+       ('svaz'),
+       ('kruh'),
+       ('meloun'),
+       ('mamut'),
+       ('krok'),
+       ('truhla'),
+       ('chlup'),
+       ('letadlo'),
+       ('kostka'),
+       ('sklo'),
+       ('trubka'),
+       ('voda'),
+       ('velryba'),
+       ('kozel'),
+       ('pudl'),
+       ('had');
